b226fc5b517f409b065a9d4cc3f5e0e4e150ec6f
Niko Mkhatvari <nico.mkhatvari@gmail.com>
2020-11-14 20:31:13 +0100

    First version of powerset

---
+++ powerset.py
@@ -0,0 +1,43 @@
+import sys
+from itertools import chain
+from itertools import combinations
+from typing import Iterable
+from typing import List
+
+
+def powerset(iterable: Iterable) -> Iterable:
+    """
+    Lazily generation of the powerset
+
+    :param iterable: Iterable of elements
+    :return: Iterable of elements
+    """
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(1, len(s) + 1))
+
+
+def parse_input(raw_input: str) -> List[str]:
+    """
+    Parses command line string transforming it into a list of strings.
+
+    :param raw_input: string to parse which respects this pattern str(,str)*
+    :return: list of strings
+    """
+    return raw_input.split(',')
+
+
+def main():
+    try:
+        elements = parse_input(sys.argv[1])
+    except IndexError:
+        sys.exit(
+            'Input argument is missing. Please provide a string complaint to the following regular expression: "[0-9a-zA-Z]+(\,[0-9a-zA-Z]+)+"')
+
+    iter_powerset = powerset(elements)
+
+    for elem_set in iter_powerset:
+        print(','.join(elem_set) + '\n')
+
+
+if __name__ == "__main__":
+    main()
