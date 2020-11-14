0f4bb46c3c894d3469c733d2755a12ebed706dbb
Niko Mkhatvari <nico.mkhatvari@gmail.com>
2020-11-14 21:41:00 +0100

    Defined first unit test

---
+++ test_powerset.py
@@ -0,0 +1,34 @@
+import unittest
+
+from powerset import format_output
+from powerset import parse_input
+from powerset import powerset
+
+
+class TestPowerset(unittest.TestCase):
+
+    def test_parse_input(self):
+        test_in = '1,2,3'
+        expected_result = ['1', '2', '3']
+        self.assertEqual(parse_input(test_in), expected_result, 'Input parser test failed!')
+
+    def test_powerset_len(self):
+        test_in = [1, 2, 3]
+        self.assertEqual(len(list(powerset(test_in))), pow(2, len(test_in)) - 1, "Length test failed!")
+
+    def test_powerset_deep_comparison(self):
+        test_in = [1, 2, 3]
+        expected_result = [(1,), (2,), (3,), (1, 2,), (1, 3), (2, 3,), (1, 2, 3,)]
+        self.assertListEqual(list(powerset(test_in)), expected_result, 'Deep comparison failed!')
+
+    def test_powerset_full(self):
+        test_in = '123,456,789'
+        expected_result = """123\n456\n789\n123,456\n123,789\n456,789\n123,456,789"""
+        parsed_input = parse_input(test_in)
+        powerset_out = powerset(parsed_input)
+        formatted_out = format_output(powerset_out)
+        self.assertMultiLineEqual(formatted_out, expected_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
