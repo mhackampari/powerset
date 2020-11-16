0997710f99ee05cfec79af6a1564f5eb08142fdd
Niko Mkhatvari <nico.mkhatvari@gmail.com>
2020-11-17 00:15:56 +0100

    Defined instance variable for testing.

--- tests/test_powerset.py
+++ tests/test_powerset.py
@@ -10,14 +10,20 @@ from powerset import powerset_pythonic
 
 class TestPowerset(unittest.TestCase):
 
+    def __init__(self, *args, **kwargs):
+        super(TestPowerset, self).__init__(*args, **kwargs)
+        self.test_in = [1, 2, 3, 4]
+        self.expected_result = [[1], [2], [3], [4], [1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4], [1, 2, 3],
+                                [1, 2, 4], [1, 3, 4], [2, 3, 4], [1, 2, 3, 4]]
+
     def test_parse_input(self):
         test_in = '1,2,3'
         expected_result = ['1', '2', '3']
         self.assertEqual(parse_input(test_in), expected_result, 'Input parser test failed!')
 
     def test_powerset_len(self):
-        test_in = [1, 2, 3]
-        self.assertEqual(len(list(powerset_pythonic(test_in))), pow(2, len(test_in)) - 1, "Length test failed!")
+        self.assertEqual(len(list(powerset_pythonic(self.test_in))), pow(2, len(self.test_in)) - 1,
+                         "Length test failed!")
 
     def test_powerset_deep_comparison(self):
         test_in = [1, 2, 3]
@@ -33,23 +39,14 @@ class TestPowerset(unittest.TestCase):
         self.assertMultiLineEqual(formatted_out, expected_result)
 
     def test_dfs_powerset(self):
-        test_in = [1, 2, 3, 4]
-        expected_result = [[1], [2], [3], [4], [1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4], [1, 2, 3], [1, 2, 4],
-                           [1, 3, 4], [2, 3, 4], [1, 2, 3, 4]]
-        self.assertCountEqual(powerset_dfs(test_in), expected_result, 'Deep comparison failed!')
+        self.assertCountEqual(powerset_dfs(self.test_in), self.expected_result, 'Deep comparison failed!')
 
     def test_powerset_mp(self):
-        test_in = [1, 2, 3, 4]
-        expected_result = [[1], [2], [3], [4], [1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4], [1, 2, 3], [1, 2, 4],
-                           [1, 3, 4], [2, 3, 4], [1, 2, 3, 4]]
-        self.assertCountEqual(powerset_mp(test_in), expected_result,
+        self.assertCountEqual(powerset_mp(self.test_in), self.expected_result,
                               'Deep comparison on multiprocessing version failed!')
 
     def test_powerset_iterative(self):
-        test_in = [1, 2, 3, 4]
-        expected_result = [[1], [2], [3], [4], [1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4], [1, 2, 3], [1, 2, 4],
-                           [1, 3, 4], [2, 3, 4], [1, 2, 3, 4]]
-        self.assertCountEqual(powerset_iterative(test_in), expected_result,
+        self.assertCountEqual(powerset_iterative(self.test_in), self.expected_result,
                               'Deep comparison on iterative version failed!')
 
 
