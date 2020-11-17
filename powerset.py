6f4511573622481f3fece46efcaea87741b21fa5
Niko Mkhatvari <nico.mkhatvari@gmail.com>
2020-11-17 02:28:10 +0100

    Catch and handle MemoryError exceptions.

--- powerset.py
+++ powerset.py
@@ -125,8 +125,15 @@ def main():
         logger.error('The passed input does not respect the following regular expression: \w+(\,\w+)*')
         sys.exit('Your input does not respect this regular expression: \w+(\,\w+)*')
 
-    iter_powerset = powerset_pythonic(elements)
-    print(format_output(iter_powerset))
+    try:
+        iter_powerset = powerset_pythonic(elements)
+        print(format_output(iter_powerset))
+    except MemoryError as e:
+        exception_name = e.__class__.__name__
+        logger.error('The program has exited due to {} exception'.format(exception_name))
+        sys.exit(
+            '{}: It is an O(n*2^n) algorithm in memory and space. Watch out for the input length. Try with less input elements.'.format(
+                str(exception_name)))
 
 
 if __name__ == "__main__":
