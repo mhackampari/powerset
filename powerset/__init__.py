eed28bddec163fdb3197b7b543e4e47339935026
Niko Mkhatvari <nico.mkhatvari@gmail.com>
2020-11-18 21:50:07 +0100

    Packaged powerset

---
+++ powerset/__init__.py
@@ -0,0 +1,6 @@
+from .powerset import format_output
+from .powerset import parse_input
+from .powerset import powerset_dfs
+from .powerset import powerset_iterative
+from .powerset import powerset_mp
+from .powerset import powerset_pythonic
