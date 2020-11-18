11f3ef07ae66471a2ff93325074bd69b32b813c2
Niko Mkhatvari <nico.mkhatvari@gmail.com>
2020-11-18 22:56:36 +0100

    Setup file for powerset

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(name='Powerset',
+      version='0.1',
+      description='Powerset challenge',
+      author='Niko Mkhatvari',
+      author_email='nico.mkhatvari@gmail.com',
+      packages=find_packages(),
+      )
